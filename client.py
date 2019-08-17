dd171263dd2b5b0812a06088566c42f7068f906f
Antonydze <antony.metrica@yandex.ru>
2019-08-17 13:11:23 +0300

    remove blocking opertarions

--- client.py
+++ client.py
@@ -1,16 +1,43 @@
 import socket
 import time
+from threading import Thread
+from collections import deque
+from select import select
+
+tasks = deque()
+stopped = {}
+
+def run_queries():
+	while any([tasks, stopped]):
+		while not tasks:
+			ready_to_read, _, _ = select(stopped,[], [])
+			for r in ready_to_read:
+				tasks.append(stopped.pop(r))
+					
+		while tasks:
+			task = tasks.popleft()
+			try:
+				sock = next(task)
+				stopped[sock] = task
+			except StopIteration:
+				print("query done")
 
 def make_request():
-	start_time = time.time()
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect(("localhost", 8000))
 	sock.send(b"GET /\n\n")
+	
+	yield sock
+	
 	resp = sock.recv(100)
 	sock.close()
-	end_time = time.time()
-	print(end_time-start_time)
 	
-	
-while True:
-	make_request()
+def requesting():	
+	while True:
+		make_request()
+
+t1 = Thread(target = requesting)
+t2 = Thread(target = requesting)
+
+t1.start()
+t2.start()
